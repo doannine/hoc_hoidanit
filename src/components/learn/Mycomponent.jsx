b2619bb9e899b11fa3a08234af028897ee6fc20f
doancongchin <doannine72@gmail.com>
2024-10-30 21:23:02 +0700

    video 34

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,6 +1,14 @@
 const MyComponent = () => {
+    // const hoidanit = "hello";  // string 
+    // const hoidanit = 25 ;    // num ber
+    // const hoidanit = true ; //Boolean
+    //const hoidanit =[1,2,3]
+    const hoidanit = {
+        name: "hiiiiii",
+        age: 24
+    }
     return (
-        <div>hello nine nha</div>
+        <div>{JSON.stringify(hoidanit)} nine nha</div>
     );
 }
 
---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,13 @@
+
+const TodoData = (props) => {
+    const { name, age, data } = props;
+    console.log(">>check props: ", props)
+    return (
+        <div className='todo-data'>
+            <div>My name is {name} </div>
+            <div>Learning react</div>
+            <div>Learning react 1</div>
+        </div>
+    )
+}
+export default TodoData;
