3caa9ea249300d49ca886a64dcdb89cb6f7a445d
doancongchin <doannine72@gmail.com>
2024-10-30 21:53:45 +0700

    video 35

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,7 @@
 
 const TodoData = (props) => {
     const { name, age, data } = props;
-    console.log(">>check props: ", props)
+    // console.log(">>check props: ", props)
     return (
         <div className='todo-data'>
             <div>My name is {name} </div>
