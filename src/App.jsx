6ea801a0afc7c060c401462b5d3963e35bb0dee3
doancongchin <doannine72@gmail.com>
2024-11-05 17:19:30 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -29,15 +29,35 @@ const App = () => {
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
+
       <TodoNew
         addNewToDo={addNewToDo}
       />
-      <TodoData
-        todoList={todoList}
-      />
-      <div className='todo-image'>
-        <img src={reactLogo} className='logo' />
-      </div>
+
+      {todoList.length > 0 ?
+        <TodoData
+          todoList={todoList}
+        />
+        :
+        <div className='todo-image'>
+          <img src={reactLogo} className='logo' />
+        </div>
+      }
+
+      {/* {todoList.length > 0 &&
+
+        <TodoData
+          todoList={todoList}
+        />
+
+      }
+
+      {todoList.length === 0 &&
+        <div className='todo-image'>
+          <img src={reactLogo} className='logo' />
+        </div>
+      } */}
+
     </div>
   )
 }
