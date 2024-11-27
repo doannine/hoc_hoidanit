3ca733a03bbef797d984ffc63732454f118a2b3e
doancongchin <doannine72@gmail.com>
2024-11-27 22:41:54 +0700

    video 67

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -30,3 +30,7 @@ const TodoNew = (props) => {
     )
 }
 export default TodoNew;
+
+
+
+
--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,30 +1,49 @@
 import { Button, Flex } from 'antd';
 
 import { Input } from "antd";
+import { useState } from 'react';
 
 
 const UserForm = () => {
+    const [fullName, setfullName] = useState("");
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [phoneNumber, setphoneNumber] = useState("");
+    const handleClickBtn = () => {
+
+    }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>FullName</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setfullName(event.target.value) }} />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => { setemail(event.target.value) }} />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setpassword(event.target.value) }} />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input />
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => { setphoneNumber(event.target.value) }} />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button
+                        //onClick={() =>handleClickBtn()} giông cách dưới nhưng cách này truyền được tham số đầu vào 
+                        onClick={handleClickBtn}
+                        type="primary">Create User</Button>
                 </div>
 
             </div>
