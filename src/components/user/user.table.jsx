0893c99445c269216947ca1966daf4224506caa7
doancongchin <doannine72@gmail.com>
2025-03-09 22:21:58 +0700

    video80

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,8 +1,9 @@
-import { Table } from 'antd';
+import { Table, notification, Popconfirm } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import UpdateUserModal from './update.user.modal';
 import { useState } from 'react';
 import ViewUserDetail from './view.user.detail';
+import { deleteUserAPI } from '../../services/api.service';
 
 
 const UserTable = (props) => {
@@ -51,7 +52,18 @@ const UserTable = (props) => {
                             SetIsModalUpdateOpen(true);
                         }}
                         style={{ cursor: "pointer", color: "orange" }} />
-                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                    <Popconfirm
+                        title="Delete user"
+                        description="Are you sure to delete user?"
+                        onConfirm={() => handleDeleteUser(record._id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="left"
+                    >
+                        <DeleteOutlined
+                            style={{ cursor: "pointer", color: "red" }}
+                        />
+                    </Popconfirm>
                 </div>
 
 
@@ -60,7 +72,22 @@ const UserTable = (props) => {
 
     ];
 
+    const handleDeleteUser = async (id) => {
+        const res = await deleteUserAPI(id);
+        if (res.data) {
+            notification.success({
+                message: "Delete user",
+                description: " xóa user thành công "
+            })
+            await loadUser();
 
+        } else {
+            notification.error({
+                message: "Error delete user",
+                description: "Xóa user k thành công"
+            })
+        }
+    }
 
 
 
