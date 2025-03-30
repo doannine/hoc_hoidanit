ce72aa19f05af6d8436335635cd4f510877bf1eb
doancongchin <doannine72@gmail.com>
2025-03-30 23:17:43 +0700

    vd89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,74 @@
 
+import { Button } from 'antd';
+
+import { Input, Form } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log("checkk>>", values);
+    }
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            name="basic"
+
+            onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <div style={{
+                // display: "flex",
+                margin: "50px",
+                // flexDirection: "column"
+            }}>
+
+
+
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <div>
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary">Register</Button>
+                </div>
+
+            </div>
+        </Form>
     )
 }
 export default RegisterPage;
