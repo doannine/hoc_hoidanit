3cdfea71b66f45decca330f3a95860551d57ac00
doancongchin <doannine72@gmail.com>
2025-03-31 22:30:13 +0700

    vd90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,12 +1,34 @@
 
-import { Button } from 'antd';
+import { Button, notification } from 'antd';
 
 import { Input, Form } from "antd";
+import { registerUserAPI } from '../services/api.service';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterPage = () => {
+
     const [form] = Form.useForm();
-    const onFinish = (values) => {
-        console.log("checkk>>", values);
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
+
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone);
+        if (res.data) {
+            notification.success({
+                message: "register user",
+                description: "dang ki thanh cong"
+            });
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "register user error",
+                description: "dang ki khong thanh cong"
+            })
+        }
     }
     return (
         <Form
@@ -29,7 +51,10 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                //rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{
+                        required: true,
+                        message: 'Please input your fullName!'
+                    }]}
                 >
                     <Input />
                 </Form.Item>
@@ -37,7 +62,10 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Email"
                     name="email"
-                //rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{
+                        required: true,
+                        message: 'Please input your email!'
+                    }]}
                 >
                     <Input />
                 </Form.Item>
@@ -47,7 +75,10 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Password"
                     name="password"
-                //rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{
+                        required: true,
+                        message: 'Please input your password!'
+                    }]}
                 >
                     <Input.Password />
                 </Form.Item>
@@ -56,7 +87,11 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                //rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{
+                        required: true,
+                        pattern: new RegExp(/\d+/g),
+                        message: "Wrong format!"
+                    }]}
                 >
                     <Input />
                 </Form.Item>
@@ -65,6 +100,7 @@ const RegisterPage = () => {
                     <Button
                         onClick={() => form.submit()}
                         type="primary">Register</Button>
+
                 </div>
 
             </div>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -64,6 +64,20 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
 }
 
 
+
+const registerUserAPI = (fullName, email, password, phone) => {
+
+    const URL_BACKEND = "/api/v1/user/register";
+    const data = {
+        fullName: fullName,
+        email: email,
+        password: password,
+        phone: phone
+    }
+    return axios.post(URL_BACKEND, data);
+
+}
+
 export {
-    createUserAPI, UpdateUserAPI, fetchAllUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI
+    createUserAPI, UpdateUserAPI, fetchAllUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI, registerUserAPI
 }
